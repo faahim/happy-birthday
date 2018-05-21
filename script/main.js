e0ebb3237e3d4744f3897c66e1f6b95b612471c2
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-05-21 23:27:37 +0600

    first complete iteration

--- script/main.js
+++ script/main.js
@@ -1,8 +1,11 @@
 const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
+const hbd = document.getElementsByClassName("wish-hbd")[0];
 
 textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
   .split("")
   .join("</span><span>")}</span`;
+
+hbd.innerHTML = `<span>${hbd.innerHTML.split("").join("</span><span>")}</span`;
 
 const ideaTextTrans = {
   opacity: 0,
@@ -118,7 +121,6 @@ tl
       z: 10,
       opacity: 0
     },
-    "currentStep",
     "+=0.5"
   )
   .to(
@@ -173,20 +175,97 @@ tl
       opacity: 1,
       y: -1000
     },
-    0.2
+    0.2,
+    "currentStep"
   )
-  .from(".lydia-dp", 0.5, {
-    scale: 3.5,
-    opacity: 0,
-    x: 25,
-    y: -25,
-    rotationZ: -45
-  })
+  .from(
+    ".lydia-dp",
+    0.5,
+    {
+      scale: 3.5,
+      opacity: 0,
+      x: 25,
+      y: -25,
+      rotationZ: -45
+    },
+    "-=2"
+  )
   .from(".hat", 0.5, {
     x: -100,
     y: 350,
     rotation: -180,
     opacity: 0
-  });
+  })
+  .staggerFrom(
+    ".wish-hbd span",
+    0.7,
+    {
+      opacity: 0,
+      y: -50,
+      // scale: 0.3,
+      rotation: 150,
+      skewX: "30deg",
+      ease: Elastic.easeOut.config(1, 0.5)
+    },
+    0.1
+  )
+  .staggerFromTo(
+    ".wish-hbd span",
+    0.7,
+    {
+      scale: 1.4,
+      rotationY: 150
+    },
+    {
+      scale: 1,
+      rotationY: 0,
+      color: "#ff69b4",
+      ease: Expo.easeOut
+    },
+    0.1,
+    "party"
+  )
+  .from(
+    ".wish h5",
+    0.5,
+    {
+      opacity: 0,
+      y: 10,
+      skewX: "-15deg"
+    },
+    "party"
+  )
+  .staggerTo(
+    ".eight svg",
+    1.5,
+    {
+      visibility: "visible",
+      opacity: 0,
+      scale: 80,
+      repeat: 3,
+      repeatDelay: 1.4
+    },
+    0.3
+  )
+  .to(".six", 0.5, {
+    opacity: 0,
+    y: 30,
+    zIndex: "-1"
+  })
+  .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+  .to(
+    ".last-smile",
+    0.5,
+    {
+      rotation: 90
+    },
+    "+=1"
+  );
 
 tl.seek("currentStep");
+
+// Restart on click
+const replyBtn = document.getElementById("replay");
+replyBtn.addEventListener("click", () => {
+  tl.restart();
+});
