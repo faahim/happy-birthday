0db870f6392f711661de0bf0cfb8556afa07d0c9
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-05-21 18:05:47 +0600

    created initial animation timeline

--- script/main.js
+++ script/main.js
@@ -1,5 +1,181 @@
-const tl = new TimelineMax()
+const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
 
-tl.from('#hbd', 0.3, {
-  y: 700,
-})
+textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
+  .split("")
+  .join("</span><span>")}</span`;
+
+const ideaTextTrans = {
+  opacity: 0,
+  y: -20,
+  rotationX: 5,
+  skewX: "15deg"
+};
+
+const ideaTextTransLeave = {
+  opacity: 0,
+  y: 20,
+  rotationY: 5,
+  skewX: "-15deg"
+};
+
+const tl = new TimelineMax();
+
+tl
+  .to(".container", 0.1, {
+    visibility: "visible"
+  })
+  .from(".one", 0.7, {
+    opacity: 0,
+    y: 10
+  })
+  .from(".two", 0.4, {
+    opacity: 0,
+    y: 10
+  })
+  .to(
+    ".one",
+    0.7,
+    {
+      opacity: 0,
+      y: 10
+    },
+    "+=2.5"
+  )
+  .to(
+    ".two",
+    0.7,
+    {
+      opacity: 0,
+      y: 10
+    },
+    "-=1"
+  )
+  .from(".three", 0.7, {
+    opacity: 0,
+    y: 10
+    // scale: 0.7
+  })
+  .to(
+    ".three",
+    0.7,
+    {
+      opacity: 0,
+      y: 10
+    },
+    "+=2"
+  )
+  .from(".four", 0.7, {
+    scale: 0.2,
+    opacity: 0
+  })
+  .from(".fake-btn", 0.3, {
+    scale: 0.2,
+    opacity: 0
+  })
+  .staggerTo(
+    ".hbd-chatbox span",
+    0.5,
+    {
+      visibility: "visible"
+    },
+    0.05
+  )
+  .to(".fake-btn", 0.1, {
+    backgroundColor: "rgb(127, 206, 248)"
+  })
+  .to(
+    ".four",
+    0.5,
+    {
+      scale: 0.2,
+      opacity: 0,
+      y: -150
+    },
+    "+=0.7"
+  )
+  .from(".idea-1", 0.7, ideaTextTrans)
+  .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+  .from(".idea-2", 0.7, ideaTextTrans)
+  .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+  .from(".idea-3", 0.7, ideaTextTrans)
+  .to(".idea-3 strong", 0.5, {
+    scale: 1.2,
+    x: 10,
+    backgroundColor: "rgb(21, 161, 237)",
+    color: "#fff"
+  })
+  .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+  .from(".idea-4", 0.7, ideaTextTrans)
+  .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+  .from(
+    ".idea-5",
+    0.7,
+    {
+      rotationX: 15,
+      rotationZ: -10,
+      skewY: "-5deg",
+      y: 50,
+      z: 10,
+      opacity: 0
+    },
+    "currentStep",
+    "+=0.5"
+  )
+  .to(
+    ".idea-5 span",
+    0.7,
+    {
+      rotation: 90,
+      x: 8
+    },
+    "+=0.4"
+  )
+  .to(
+    ".idea-5",
+    0.7,
+    {
+      scale: 0.2,
+      opacity: 0
+    },
+    "+=2"
+  )
+  .staggerFrom(
+    ".idea-6 span",
+    0.8,
+    {
+      scale: 3,
+      opacity: 0,
+      rotation: 15,
+      ease: Expo.easeOut
+    },
+    0.2
+  )
+  .staggerTo(
+    ".idea-6 span",
+    0.8,
+    {
+      scale: 3,
+      opacity: 0,
+      rotation: -15,
+      ease: Expo.easeOut
+    },
+    0.2,
+    "+=1"
+  )
+  .from(".lydia-dp", 0.5, {
+    scale: 3.5,
+    opacity: 0,
+    x: 25,
+    y: -25,
+    rotationZ: -45,
+    boxShadow: "0px 0px 20px 20px red"
+    // transformPerspective: 500
+  })
+  .from(".hat", 0.5, {
+    x: -100,
+    y: 350,
+    rotation: -180,
+    opacity: 0
+  });
+
+tl.seek("currentStep");
