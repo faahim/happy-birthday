41d355aec7b02aab85d86f8e26efd80e9f3a3396
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-02-02 12:31:41 +0700

    remove the hat

--- script/main.js
+++ script/main.js
@@ -220,12 +220,12 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
-      rotation: -180,
-      opacity: 0
-    })
+    // .from(".hat", 0.5, {
+    //   x: -100,
+    //   y: 350,
+    //   rotation: -180,
+    //   opacity: 0
+    // })
     .staggerFrom(
       ".wish-hbd span",
       0.7,
